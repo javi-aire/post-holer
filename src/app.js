5f3d3c1a38aeef7b1fc8d0c7e6c8937a3f9eac86
Javier Williams <javier.williams720@gmail.com>
2017-06-21 00:31:27 -0700

    Basic layout complete, response data successfully sent back and displaying on page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('it working');
+const  searchInput = document.querySelector('.app__search--input'),
+  searchButton = document.querySelector('.app__search-btn');
+let limit = document.querySelector('[name=results-limit]'),
+  filters = {
+    hot: false,
+    top: false,
+    new: false,
+    comment: false
+  };
+
+searchButton.addEventListener('click', () => {
+  const subreddit = document.querySelector('.app__search--subreddit');
+  console.log('subreddit:', subreddit.value);
+
+  fetch(`https://reddit.com/r/${subreddit.value}${searchInput.value ? 'search/'+searchInput.value : '.json'}`)
+    .then(response => { return response.json(); })
+    .then(json => {
+      document.body.innerHTML = `<div class="container">${JSON.stringify(json.data)}</div>`;
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})
+
+console.log(`${limit}`);
